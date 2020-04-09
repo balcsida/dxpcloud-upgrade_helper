848bc44b3442bdbfe1c9651688d0046847be00bb
Dávid Balatoni <balcsida@gmail.com>
2020-04-09 13:45:16 +0200

    Create environment preparing script

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ This tool helps you to upgrade your DXP Cloud environment to 7.2
 
 ## Usage
 
+0. Install prerequisites (see `prepare_environment.sh`)
 1. Download repository ZIP: https://github.com/balcsida/dxpcloud-upgrade_helper/archive/master.zip
 2. Extract ZIP
 3. Execute `./upgrade.sh dxpcloud_project_name` inside the extracted directory
---
+++ prepare_environment.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install jq -y
+
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+sudo usermod -aG docker $USER
+echo "Please close this Shell window"
